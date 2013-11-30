2a8187c871c3c10a0fb984699af94a845c3b8afa
user <ghghg@ukr.net>
2013-11-30 13:40:07 +0200

    first test

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+
+public class Test1 {
+    @Test
+    public void testMaxTrue(){
+        assertEquals(10,Max.Max(4,6));
+
+    }
+
+
+}
